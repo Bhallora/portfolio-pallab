153509d20a48212ef039887d420972656e4e129b
Pallab Borah <pallab.borah@kdi.kongsberg.com>
2020-06-12 13:17:47 +0530

    spinner changes included

--- src/app/about-experience/about-experience.component.ts
+++ src/app/about-experience/about-experience.component.ts
@@ -10,14 +10,6 @@ export class AboutExperienceComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.content = 'Hello everyone! My name is Pallab Borah. '
-   + 'Currently, I am working as a front-end developer in Kongsberg Digital India. ' 
-   + 'Prior to joining Kongsberg, I worked in the Social Development Sector for a year as a fellow in Teach For India. ' 
-   + 'In addition to these, I also have sales & marketing Internship experience with OYO. ' 
-   + 'As far my educational qualification is concerned I have completed my BTech in CSE from Tezpur Central University. '
-   + 'I love being a techie but at the same time I am quite fascinated by the principles of Design. '
-    + 'P.S.  I am a chai lover and a storyteller. '
- + ' So, If you are up for a conversation over a cup of tea, please contact me at any one the social media handles attached with the photo';
   }
 
 }
